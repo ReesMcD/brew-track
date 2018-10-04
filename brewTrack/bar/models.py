cb4853fb6bfeddc8b76f93c9d9b5a4c8455bdbf1
Mark Granatire <markgranatire@Marks-MacBook-Pro.local>
2018-10-04 15:48:12 -0400

    Models Structure

--- brewTrack/bar/models.py
+++ brewTrack/bar/models.py
@@ -5,3 +5,40 @@ from django.db import models
 class Bar(models.Model):
     name = models.CharField(max_length=20)
     pub_date = models.DateTimeField('date published')
+# bar model
+# location (addresses string)
+# menu (list)
+# hours 
+# cover price (int list)
+# weekly calendar list (list)****
+# specials (list)
+# admin list 
+# tab list (list (string))
+# transcation list
+
+
+# drinks model
+# name 
+# alcholic 
+# non alcoholic
+# ingridents (optional)
+# price (optional)
+# alcohol content (optional)
+# craft beer (location) (optional)
+# type (subtype) (optional)
+
+
+# event model
+# name 
+# type
+# description
+# date
+# ticket 
+# non ticket
+
+
+# user model
+# username
+# password
+# admin list
+

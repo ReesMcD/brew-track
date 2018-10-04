21d8a6e94edad38936f9c348a8908f7c090b9026
ReesMcD <mcdevittrees@gmail.com>
2018-10-04 16:21:43 -0400

    Added model stuff

--- brewTrack/bar/models.py
+++ brewTrack/bar/models.py
@@ -2,12 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Bar(models.Model):
-    name = models.CharField(max_length=20)
-    pub_date = models.DateTimeField('date published')
-
-
-
 # bar model
 # location (addresses string)
 # menu (list)
@@ -18,29 +12,39 @@ class Bar(models.Model):
 # admin list
 # tab list (list (string))
 # transcation list
+class Bar(models.Model):
+    name = models.CharField(max_length=20)
+    pub_date = models.DateTimeField('date published')
+    location = models.CharField(max_length=100)
+    hours = models.CharField(max_length=50)
+
+# Drink Model
+class Drink(models.Model):
+    name = models.CharField(max_length=20)
+    alcholic = models.BooleanField(Defult=True)
+    ingridents = models.CharField(max_length=500) # Possibly make this its own model eventually
+    alcholic_content = models.CharField(max_length=100)
+    location = models.CharField(max_length=30)
+    type = models.CharField(max_length=20)
+    
+
+#Menu Model
+class Menu(models.Model):
+    name = models.CharField(max_length=20)
+    #Many to Many with drinks
 
 
-# drinks model
-# name
-# alcholic
-# non alcoholic
-# ingridents (optional)
-# price (optional)
-# alcohol content (optional)
-# craft beer (location) (optional)
-# type (subtype) (optional)
-
-
-# event model
-# name
-# type
-# description
-# date
-# ticket
-# non ticket
-
+# Event model
+class Event(models.Model):
+    name = models.CharField(max_length=20)
+    type = models.CharField(max_length=20)
+    description = models.CharField(max_length=100)
+    date = models.DateTimeField()
+    ticket = models.BooleanField(default=False)
 
 # user model
 # username
 # password
 # admin list
+class User(models.Model):
+    name = models.CharField(max_length=20)

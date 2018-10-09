a4c044642ede37ba7243b8a06c43c097a96f8aa8
ReesMcD <mcdevittrees@gmail.com>
2018-10-09 18:51:54 -0400

    Added static CSS file and added it to header, created basic test to test 200 status code on homepage

--- brewTrack/bar/tests.py
+++ brewTrack/bar/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from bar.models import Bar
 
 # Create your tests here.
+class Bar_Pages_Test(TestCase):
+    # def setUp(self):
+    #     Bar.objects.create(name="Test Bar")
+
+    def test_bar_home(self):
+        print("Testing for bar...")
+        response = self.client.get('/bar/', follow=True)
+        self.assertEqual(response.status_code, 200)

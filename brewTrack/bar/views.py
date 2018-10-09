c16929455bd491e8852887ba88f0f0a46353b7fd
ReesMcD <mcdevittrees@gmail.com>
2018-10-09 18:56:15 -0400

    Cleaning up

--- brewTrack/bar/views.py
+++ brewTrack/bar/views.py
@@ -5,16 +5,22 @@ from .models import Bar
 
 def index(request):
     bar_list = Bar.objects.order_by('id')
-    # template = loader.get_template('bar/index.html')
+
     context = {
         'bar_list': bar_list,
     }
+
     return render(request, 'bar/index.html', context)
 
 def bar(request, bar_id):
     bar = get_object_or_404(Bar, pk=bar_id)
     response = "You're looking at bar %s."
-    return render(request, 'bar/barhome.html', {'bar': bar})
+
+    context = {
+        'bar': bar,
+    }
+
+    return render(request, 'bar/bar_page.html', context)
 
 def next(request, bar_id):
     return HttpResponse("You're look at another page for %s." % bar_id)
